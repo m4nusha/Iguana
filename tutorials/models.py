282bb515eb3ae43f8732aa62f7a4b57b8eb09340
Mozon <mozonibrahim@hotmail.com>
2024-12-11 10:33:28 +0000

    Resolve conflicts

--- tutorials/models.py
+++ tutorials/models.py
@@ -8,6 +8,7 @@ from django.db import models
 from datetime import datetime, timedelta, date, time
 
 
+
 class User(AbstractUser):
     """Model used for user authentication, and team member related information."""
 
